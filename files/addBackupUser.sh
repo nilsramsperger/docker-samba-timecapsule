21a0bca67346a3fe06b787f6cbea6bb81a7f2b65
nils <nils@ramsperger-home.de>
2019-03-28 19:39:25 +0100

    Added usage info. Added request for new password.

--- files/addBackupUser.sh
+++ files/addBackupUser.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env ash
-adduser $1 --shell /bin \
-&& smbpasswd -a $1 \
-&& mkdir -m 700 /var/data/timemachine/$1 \
-&& cp /temp/.com.apple.TimeMachine.quota.plist /var/data/timemachine/$1/.com.apple.TimeMachine.quota.plist \
-&& chown -r $1 /var/data/timemachine/$1
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: addBackupUser [username]"
+    exit 1
+else
+    echo "Enter new password"
+    read -s pw1
+    echo "Confirm password"
+    read -s pw2
+    if [[ "$pw1" = "$pw2" ]]; then
+        yes "$pw1" | adduser $1 --shell /bin \
+        && yes "$pw1" | smbpasswd -a $1 \
+        && mkdir -m 700 /var/data/timemachine/$1 \
+        && cp /temp/.com.apple.TimeMachine.quota.plist /var/data/timemachine/$1/.com.apple.TimeMachine.quota.plist \
+        && chown -R $1 /var/data/timemachine/$1 \
+        && echo "TimeCapsule user added" \
+        && exit 0
+    else
+        echo "Passwords missmatch"
+        exit 1
+    fi
+fi
